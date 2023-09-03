15f326d875c86d0c9aa6ab8841c9cb82ac79cd9d
JesusLazoTapia <u202019038@upc.edu.pe>
2023-09-03 09:32:09 -0500

    NetTeam Plantilla

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script setup>
+import WelcomeNetTeam from "@/components/WelcomeNetTeam.vue";
+import DescriptionNet from "@/components/DescriptionNet.vue";
+</script>
+
+<template>
+  <header>
+    <img alt="NetTeamLogo" class="logo" src="./assets/logoNetTeam.png" width="340" height="210" />
+
+    <div class="wrapper">
+      <WelcomeNetTeam msg="Hello user" />
+    </div>
+  </header>
+
+  <main>
+    <DescriptionNet />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/DescriptionNet.vue
@@ -0,0 +1,40 @@
+<script setup>
+
+import WelcomeItem from "@/components/WelcomeItem.vue";
+import DocumentationIcon from "@/components/icons/IconDocumentation.vue";
+import CommunityIcon from "@/components/icons/IconCommunity.vue";
+import SupportIcon from "@/components/icons/IconSupport.vue";
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Documentation</template>
+
+    NetTeam It offers them a wide variety of options that they might like.
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+    <template #heading>Community</template>
+
+    Connect with your gastronomic community and like-minded entrepreneurs. We amplify communication between foodies and culinary entrepreneurs.
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <template #heading>Support NetTeam</template>
+
+    Your privacy is our priority. We are committed to protecting your personal data and providing you with communication channels to report any incident.
+  </WelcomeItem>
+</template>
+
+<style scoped>
+
+</style>
